40d24fb3ecb506f23181c9363dcc636700a57fcd
Vladimir Chekhlov <chekhlov51@gmail.com>
2022-03-27 13:22:50 +0300

    one step left(test incomplite)

--- tests/test_class_Posts.py
+++ tests/test_class_Posts.py
@@ -1,6 +1,4 @@
-import pytest
 from main import app
-from utils import Posts
 from flask import Flask, jsonify
 
 app.config['JSON_AS_ASCII'] = False
@@ -8,37 +6,30 @@ DATA_PATH = 'data/data.json'
 app = Flask(__name__)
 
 
-@app.route('/posts')
+@app.route('/api/posts')
 def test_text():
     return "it is ok"
 
 
-@app.route('/users/leo')
+@app.route('/api/posts/15')
 def json_key():
-    data = {'name': 'leo'}
+    data = {'poster_name': 'leo'}
     return jsonify(data)
 
 
-class TestPosts:
-    def test_get_posts_all(self):
-        """
-        Тестирование на правильное соединение с сервером при выводе всех постов, на возврат списка, значений ключа
+def test_get_posts_all():
+    response = app.test_client().get('api/posts/15')
 
-        """
-        response = app.test_client().get('/posts')
-        temp = []
-        assert response.status_code == 200
-        assert response.data == b'it is ok'
-        assert type(temp) == type(response.json), 'Вернулся не список'
-        assert 'poster_name' in response.json[0], 'Нет ключа к "poster_name"'
+    assert response.status_code == 404
+    assert set(response.json.keys()) == {
+        'content', 'pic', 'likes_count', 'pk', 'poster_name', 'poster_avatar', 'views_count'
+    }
+    assert response.json == {'error': "Пост не найден"}
 
-    def test_get_post_json(self):
-        """
-        Проверка на возврат словаря, ключей словаря.
 
-        """
-        temp = {}
-        response = app.test_client().get('/users/leo')
-        assert response.json.get("name") == "leo", 'Incorrect name'
-        assert type(temp) == type(response.json), 'Вернулся не словарь'
+def test_get_post():
+    response = app.test_client().get('/api/posts')
 
+    assert response.status_code == 200
+    assert isinstance(response.json, list)
+    assert len(response.json) > 0
